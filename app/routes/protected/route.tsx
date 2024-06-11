cc866aa3d5a52178e4186b8da630250e1badecf0
Josiah Griggs <josiahstanford8@gmail.com>
2024-06-11 13:09:27 -0700

    added toasts and cleaned up

--- app/routes/protected/route.tsx
+++ app/routes/protected/route.tsx
@@ -2,29 +2,16 @@ import {
   ActionFunctionArgs,
   json,
   LoaderFunctionArgs,
+  redirect,
 } from "@remix-run/cloudflare";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
 import { drizzle } from "drizzle-orm/d1";
 import { resources } from "app/drizzle/schema.server";
-
-import {
-  S3Client,
-  ListObjectsV2Command,
-  GetObjectCommand,
-  PutObjectCommand,
-} from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { Navbar } from "../app/navbar";
+import { useToast } from "~/components/ui/use-toast";
 import { doTheAuthThing } from "lib/authThing";
-
-const S3 = new S3Client({
-  region: "auto",
-  endpoint: `https://bbe111b6726945b110b32ab037e4c232.r2.cloudflarestorage.com`,
-  credentials: {
-    accessKeyId: "e74dc595a3b18668b5e9f6795929cf3c",
-    secretAccessKey:
-      "b3c68d4ced82ad17a964d22648266c0a0b6fc55d0cf8b5f775e1183b4616b065",
-  },
-});
+import { useEffect, useRef } from "react";
+import { Button } from "~/components/ui/button";
 
 // This function fetches resources from D1 and images from R2
 export async function loader({ request, context }: LoaderFunctionArgs) {
@@ -33,140 +20,31 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
   // now you just have to condition all these queires on the user id
   if (user) {
     const userId = user.id;
-    console.log("LOGGED IN!!");
-    //      ^ type: string
+    console.log("LOGGED IN!!")
+    return(true);
+
   } else {
     console.log("NOT LOGGED IN!!");
+    return(false);
   }
-  const resourceList = await db
-    .select({
-      id: resources.id,
-      title: resources.title,
-      href: resources.href,
-    })
-    .from(resources)
-    .orderBy(resources.id);
-
-  const { Contents } = await S3.send(
-    new ListObjectsV2Command({ Bucket: "artworks" })
-  );
-
-  const imageList = await Promise.all(
-    Contents?.map(async (file) => {
-      if (file.Key) {
-        return getSignedUrl(
-          S3,
-          new GetObjectCommand({
-            Bucket: "artworks",
-            Key: file.Key,
-          }),
-          { expiresIn: 3600 }
-        ); // Expires in 1 hour
-      }
-      return null;
-    }) || []
-  );
-
-  console.log(await S3.send(new ListObjectsV2Command({ Bucket: "artworks" })));
-  return json({
-    resourceList,
-    imageList: imageList.filter((url) => url !== null), // Pass the list of signed image URLs to the frontend
-  });
 }
 
-export default function Index() {
-  const { resourceList, imageList } = useLoaderData<typeof loader>();
+export default function Protected() {
+  const data = useLoaderData<typeof loader>();
+  const { toast } = useToast();
+  const toastShownRef = useRef(false);
+
+  if (!toastShownRef.current) {
+    toastShownRef.current = true;
+    toast({
+      title: data ? "Logged In" : "Not Logged In",
+      description: data ? "You are logged in." : "You are not logged in.",
+      variant: data ? "default" : "destructive",
+    });
+  }
 
   return (
-    <div style={{ width: "500px", height: "auto", overflow: "hidden" }}>
-      <h1>Welcome to Remix (with Drizzle, Vite, and Cloudflare D1 and R2)</h1>
-      <h2>Image Gallery</h2>
-      <ul>
-        {imageList.map((url, index) => (
-          <li key={index}>
-            <img
-              src={url}
-              alt={`Artwork ${index}`}
-              style={{ width: "100%", height: "auto" }}
-            />
-          </li>
-        ))}
-      </ul>
-      <Form method="post" encType="multipart/form-data">
-        <div>
-          <label>
-            Upload Image: <input type="file" name="image" required />
-          </label>
-        </div>
-        <button type="submit">Upload</button>
-      </Form>
-      <ul>
-        {resourceList.map((resource) => (
-          <li key={resource.id}>
-            <a target="_blank" href={resource.href} rel="noreferrer">
-              {resource.title}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <Form method="POST">
-        <div>
-          <label>
-            Title: <input type="text" name="title" required />
-          </label>
-        </div>
-        <div>
-          <label>
-            URL: <input type="url" name="href" required />
-          </label>
-        </div>
-        <button type="submit">Add Resource</button>
-      </Form>
+    <div className="flex items-center justify-center">
     </div>
   );
-}
-
-export async function action({ request, context }: ActionFunctionArgs) {
-  const formData = await request.formData();
-  const db = drizzle(context.cloudflare.env.DB);
-
-  // Handle resource addition
-  const title = formData.get("title");
-  const href = formData.get("href");
-  if (title && href) {
-    await db
-      .insert(resources)
-      .values({ title: title as string, href: href as string })
-      .execute();
-    return json({ message: "Resource added" }, { status: 201 });
-  }
-
-  // Handle image upload
-  const file = formData.get("image");
-  if (file instanceof File) {
-    // Prepare the payload for the S3 upload
-    const fileStream = file.stream();
-    const fileName = file.name;
-    const fileType = file.type;
-
-    try {
-      await S3.send(
-        new PutObjectCommand({
-          Bucket: "artworks", // Specify your S3 bucket name
-          Key: fileName,
-          Body: fileStream,
-          ContentType: fileType,
-        })
-      );
-      return json(
-        { message: "Image uploaded to S3 successfully" },
-        { status: 201 }
-      );
-    } catch (error) {
-      console.error("Failed to upload to S3", error);
-      return json({ message: "Failed to upload image" }, { status: 500 });
-    }
-  }
-
-  return json({ message: "No operation performed" }, { status: 400 });
 }
